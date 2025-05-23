9dad4e6554472a27134c52fc00a3ac7c4428de37
Per1ova <azzabimourad2@gmail.com>
2025-05-23 13:19:13 +0200

    Aufgabe 1

--- lib/input_counter_box.dart
+++ lib/input_counter_box.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
 class InputCounterBox extends StatefulWidget {
-  const InputCounterBox({super.key});
+  final Color color;
+  final String print;
+  const InputCounterBox({super.key, required this.color, required this.print});
 
   @override
   State<InputCounterBox> createState() => _InputCounterBoxState();
 }
 
 class _InputCounterBoxState extends State<InputCounterBox> {
+  TextEditingController inputController = TextEditingController();
   int _length = 0;
+
+  void printChanged(String name) {
+    debugPrint("Box $name wurde geändert");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: widget.color,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
+              controller: inputController,
               onChanged: (text) {
                 setState(() {
                   _length = text.length;
+                  printChanged(widget.print);
                 });
               },
               decoration: InputDecoration(
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,20 +1,34 @@
 import 'package:callback_functions/input_counter_box.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           spacing: 16,
           children: [
-            InputCounterBox(),
-            InputCounterBox(),
-            InputCounterBox(),
+            InputCounterBox(
+              color: Colors.lightGreen, print: '1',
+            
+              
+            ),
+            InputCounterBox(
+              color: Colors.lightBlue, print: '2',
+            ),
+            InputCounterBox(
+              color: Colors.purple, print: '3',
+            ),
           ],
         ),
       ),
